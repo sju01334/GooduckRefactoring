28124b95b1faaec84b4ce4d2035151f02ef66c85
sju01334 <sju01334@gmail.com>
2022-09-14 13:04:20 +0900

    CoordinatorLayout 을 이용한 사진 접히고 tab 붙이기

--- app/src/main/java/com/gooduckrefactoring/view/ProductDetailActivity.kt
+++ app/src/main/java/com/gooduckrefactoring/view/ProductDetailActivity.kt
@@ -13,14 +13,14 @@ class ProductDetailActivity : BaseActivity<ActivityProductDetailBinding>() {
     override val layoutId: Int = R.layout.activity_product_detail
 
     lateinit var mProduct : Product
-    lateinit var mPagerAdapter : ProductDetailViewPagerAdapter
+    private lateinit var mPagerAdapter : ProductDetailViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mProduct = intent.getSerializableExtra("product") as Product
 
-
+        initAdapter()
         setValues()
         setupEvents()
     }
@@ -34,18 +34,28 @@ class ProductDetailActivity : BaseActivity<ActivityProductDetailBinding>() {
             .centerCrop()
             .into(binding.productImg)
 
+        binding.productName.text =  "[미진 브랜드] 미진${ mProduct.name }"
+        binding.productName2.text = "[미진 브랜드] 미진${ mProduct.name }"
+        binding.price.text = mProduct.price.toString()
+
+
+
+
+
+    }
+
+    private fun initAdapter() {
         mPagerAdapter = ProductDetailViewPagerAdapter(this)
         binding.productDetailViewPager.adapter = mPagerAdapter
 
-        TabLayoutMediator(binding.productDetailTabLayout, binding.productDetailViewPager){ tab, position ->
-            when(position){
+        TabLayoutMediator(binding.productDetailTabLayout, binding.productDetailViewPager) { tab, position ->
+            when (position) {
                 0 -> tab.text = "상품설명"
                 1 -> tab.text = "상세정보"
                 2 -> tab.text = "후기"
                 else -> tab.text = "문의"
             }
         }.attach()
-
     }
 
     override fun initAppbar() {
